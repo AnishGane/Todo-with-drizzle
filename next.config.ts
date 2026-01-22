b1efe46becda9387a40642b380ed10272108ddd1
Anish Gane <anishgane10@gmail.com>
2026-01-22 13:39:32 +0545

    Update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
 };
 
 export default nextConfig;
